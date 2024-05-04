7c2997bb50d7429b15deb3e69785a1f65be628e4
Hash6232 <pettan@tuta.io>
2024-05-04 07:55:20 +0200

    Migrate to rollup

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+// rollup.config.js
+import typescript from '@rollup/plugin-typescript';
+import metablock from 'rollup-plugin-userscript-metablock';
+import terser from '@rollup/plugin-terser';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'build/4chan-x-yt-playlist.user.js',
+      format: 'es'
+    },
+    plugins: [typescript(), metablock({
+      file: 'src/metadata.json'
+    })]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'build/4chan-x-yt-playlist.min.user.js',
+      format: 'es'
+    },
+    plugins: [typescript(), terser(), metablock({
+      file: 'src/metadata.json'
+    })]
+  }
+];
