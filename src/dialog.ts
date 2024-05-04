11fe7c0ef6d90cb80e6bd9be4830cd9bb0639449
Hash6232 <pettan@tuta.io>
2024-05-04 11:09:51 +0200

    Fix failing dialog init and refactor

--- src/dialog.ts
+++ src/dialog.ts
@@ -1,6 +1,8 @@
+import { Playlist } from "./playlist";
+
 class Dialog {
 
-    constructor() {
+    constructor(playlist: Playlist) {
 
         document.body.insertAdjacentHTML("beforeend", "<div id=\"playlist-embed\" class=\"dialog hide\"><div><ul class=\"tabs\"></ul><div class=\"move\"></div><a class=\"jump\" href=\"javascript:;\" title=\"Jump to post\">→</a><a class=\"close\" href=\"javascript:;\" title=\"Close\">×</a></div><div id=\"playlist\"></div></div>");
 
@@ -10,12 +12,10 @@ class Dialog {
                 // Retrieve last saved position from storage
                 this.setPos(GM_getValue("4chan Dialog Coordinates", [ "10%", "5%", null, null ]));
 
-                const quickReply = document.getElementById("shortcut-qr");
-
-                if (! quickReply) throw new Error("Unable to append toggle button.");
-
-                quickReply.insertAdjacentHTML("beforebegin", "<span id=\"shortcut-playlist\" class=\"shortcut brackets-wrap\"><a id=\"playlist-toggle\" class=\"disabled\" title=\"Toggle YouTube playlist\" href=\"javascript:;\"><svg xmlns=\"http://www.w3.org/2000/svg\" class=\"icon\" aria-hidden=\"true\" focusable=\"false\" viewBox=\"0 0 512 512\"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill=\"currentColor\" d=\"M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm0 394c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V192h416v234z\"/></svg></a></span>");
+                // Add dialog toggle
+                document.getElementById("shortcut-qr")?.insertAdjacentHTML("beforebegin", "<span id=\"shortcut-playlist\" class=\"shortcut brackets-wrap\"><a id=\"playlist-toggle\" class=\"disabled\" title=\"Toggle YouTube playlist\" href=\"javascript:;\"><svg xmlns=\"http://www.w3.org/2000/svg\" class=\"icon\" aria-hidden=\"true\" focusable=\"false\" viewBox=\"0 0 512 512\"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill=\"currentColor\" d=\"M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm0 394c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V192h416v234z\"/></svg></a></span>");
                 break;
+                
 
             case "warosu.org":
                 // Retrieve last saved position from storage
@@ -33,6 +33,142 @@ class Dialog {
 
         }
 
+        // Dialog topbar
+        const move = this.node?.querySelector(".move") as HTMLElement;
+        const jump = this.node?.querySelector(".jump") as HTMLAnchorElement;
+        const close = this.node?.querySelector(".close") as HTMLAnchorElement;
+
+        move.addEventListener("mousedown", toggleDrag);
+        jump.addEventListener("click", () => { playlist.jumpTo(playlist.track) });
+        close.addEventListener("click", () => { this.toggle(true)});
+
+        // Dialog drag defuser
+        document.addEventListener("mouseup", toggleDrag);
+
+        // Dialog tabs events
+        const tabs = this.node?.querySelector("ul") as HTMLUListElement;
+        tabs.addEventListener("click", (e) => {
+            if (! playlist.player) return;
+            if (! e.target || (e.target as HTMLElement).tagName !== "A") return;
+            const index = (e.target as HTMLElement).dataset.page || "0";
+            playlist.player.cuePlaylist(playlist.toPages()[parseInt(index)])
+        });
+
+        // Dialog toggle
+        this.shortcut?.addEventListener("click", () => {
+            if (! playlist.dialog || playlist.checking || playlist.isEmpty()) return;
+            if (! playlist.player) return playlist.initAPI();
+
+            return this.toggle()
+        })
+
+        // Drag callback functions
+
+        function toggleDrag(e: MouseEvent) {
+
+            if (! playlist.dialog || ! playlist.dialog.node) return;
+        
+            switch(e.type) {
+                case "mouseup":
+                    if (! playlist.dialog.node.classList.contains("dragging")) return;
+        
+                    playlist.dialog.node.classList.remove("dragging");
+                    document.removeEventListener("mousemove", moveDialog);
+        
+                    let key: string;
+                    switch (location.hostname) {
+                        case "boards.4chan.org":
+                            key = "4chan Dialog Coordinates";
+                            break;
+                        case "warosu.org":
+                            key = "Warosu Dialog Coordinates";
+                            break;
+                        default:
+                            return;
+                    }
+        
+                    GM_setValue(key, [ 
+                        playlist.dialog.node.style.top, playlist.dialog.node.style.right, 
+                        playlist.dialog.node.style.bottom, playlist.dialog.node.style.left 
+                    ]);
+        
+                    break;
+        
+                case "mousedown":
+                    if (e.button !== 0) return;
+        
+                    e.preventDefault(); // avoid selection while dragging
+                    
+                    const rect = playlist.dialog.node.getBoundingClientRect();
+        
+                    playlist.dialog.size[0] = rect.width;
+                    playlist.dialog.size[1] = rect.height;
+        
+                    playlist.dialog.cursorPos[0] = e.x - rect.x;
+                    playlist.dialog.cursorPos[1] = e.y - rect.y;
+        
+                    if (document.getElementById("header-bar")) {
+                        playlist.dialog.header[0] = document.getElementById("header-bar")?.getBoundingClientRect().y || 0;
+                        playlist.dialog.header[1] = document.getElementById("header-bar")?.offsetHeight || 0;
+                        playlist.dialog.header[2] = document.documentElement.classList.contains("fixed");
+                        playlist.dialog.header[3] = document.documentElement.classList.contains("autohide");
+                    }
+        
+                    playlist.dialog.node.classList.add("dragging");
+                    document.addEventListener("mousemove", moveDialog);
+        
+                    break;
+            }
+        
+        }
+        
+        function moveDialog(e: MouseEvent) {
+        
+            if (! playlist.dialog || ! playlist.dialog.node) return;
+        
+            e.preventDefault(); // avoid selection while dragging
+        
+            const sW = document.documentElement.clientWidth,
+                sH = document.documentElement.clientHeight,
+                x = e.x - playlist.dialog.cursorPos[0],
+                y = e.y - playlist.dialog.cursorPos[1],
+                w = playlist.dialog.size[0],
+                h = playlist.dialog.size[1],
+                maxX = sW - w;
+        
+            let minY = 0, maxY = sH - h;
+        
+            if (document.getElementById("header-bar")) {
+                const fixed = playlist.dialog.header[2];
+                const autohide = playlist.dialog.header[3];
+        
+                if (fixed && ! autohide) {
+                    if (playlist.dialog.header[0] < playlist.dialog.header[1]) {
+                        minY += playlist.dialog.header[1];
+                    } else {
+                        maxY -= playlist.dialog.header[1];
+                    }
+                }
+            }
+        
+            if (y > maxY) {
+                playlist.dialog.node.style.bottom = 0 + "px";
+                playlist.dialog.node.style.removeProperty("top");
+            } else {
+                playlist.dialog.node.style.top = y > minY ? ((y * 100) / sH) + "%" : minY + "px";
+                playlist.dialog.node.style.removeProperty("bottom");
+            }
+            
+            if (x > maxX) {
+                playlist.dialog.node.style.right = 0 + "px";
+                playlist.dialog.node.style.removeProperty("left");
+            } else {
+                playlist.dialog.node.style.left = x > 0 ? ((x * 100) / sW) + "%" : 0 + "px";
+                playlist.dialog.node.style.removeProperty("right");
+            }
+        
+        }
+
     }
 
     size: [ w: number, h: number ] = [ 0, 0 ];
@@ -43,22 +179,18 @@ class Dialog {
         return document.getElementById("playlist-embed")
     }
 
-    get toggle() {
+    get shortcut() {
         return document.getElementById("playlist-toggle")
     }
 
-    open() {
-        if (! this.node || ! this.toggle) return;
-
-        this.node.classList.remove("hide");
-        this.toggle.classList.remove("disabled");
-    }
-
-    close() {
-        if (! this.node || ! this.toggle) return;
-
-        this.node.classList.add("hide");
-        this.toggle.classList.add("disabled");
+    toggle(close?: boolean) {
+        if (close || this.node?.classList.contains("hide")) {
+            this.node?.classList.remove("hide");
+            this.shortcut?.classList.remove("disabled");
+        } else {
+            this.node?.classList.add("hide");
+            this.shortcut?.classList.add("disabled");
+        }
     }
 
     updateTabs(amount: number) {
--- src/index.ts
+++ src/index.ts
@@ -1,136 +1,20 @@
 import { Playlist } from "./playlist";
+import { Dialog } from "./dialog";
 
 const playlist = new Playlist();
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Link dialog events to playlist
-    
-    const move = playlist.dialog?.node?.querySelector(".move") as HTMLElement | null;
-    const jump = playlist.dialog?.node?.querySelector(".jump") as HTMLElement | null;
-    const close = playlist.dialog?.node?.querySelector(".close") as HTMLElement | null;
 
-    move?.addEventListener("mousedown", toggleDrag);
-    jump?.addEventListener("click", () => { playlist.findPost(playlist.track)?.scrollIntoView() });
-    close?.addEventListener("click", () => { playlist.dialog?.close()})
-
-    document.addEventListener("mouseup", toggleDrag);
-
-    playlist.dialog?.node?.querySelector("ul")?.addEventListener("click", (e) => {
-        if (! playlist.player) return;
-        if (! e.target || (e.target as HTMLElement).tagName !== "A") return;
-        const index = (e.target as HTMLElement).dataset.page || "0";
-        playlist.player.cuePlaylist(playlist.toPages()[parseInt(index)])
-    });
+    if (location.hostname === "warosu.org") playlist.dialog = new Dialog(playlist);
 
     document.head.insertAdjacentHTML("beforeend", "<style>" + "#playlist-embed{position:fixed;padding:1px 4px 1px 4px}#playlist-embed.hide{display:none}#playlist-embed>div:first-child{display:flex}#playlist-embed ul,#playlist-embed li{margin:0;padding:0}#playlist-embed li{display:inline-block;list-style-type:none}#playlist-embed li:not(:only-of-type):not(:last-of-type)::after{content:'•';margin:0 0.25em}#playlist-embed .move{flex:1}#playlist-embed .jump{margin-top:-1px}#playlist-embed .close{margin-left:4px}" + (location.hostname === "boards.4chan.org" ? ":root:not(.fourchan-xt) #shortcut-playlist .icon{height:15px;width:16px;margin-bottom:-3px;}" : ".dialog{background-color:var(--darker-background-color);border:1px solid var(--even-darker-background-color);box-shadow: 0 1px 2px rgba(0, 0, 0, .15);}#playlist-embed a{text-decoration:none;}#playlist-embed .move{cursor:move;}") + "</style>");
 
-    function toggleDrag(e: MouseEvent) {
-
-        if (! playlist.dialog || ! playlist.dialog.node) return;
-    
-        switch(e.type) {
-            case "mouseup":
-                if (! playlist.dialog.node.classList.contains("dragging")) return;
-    
-                playlist.dialog.node.classList.remove("dragging");
-                document.removeEventListener("mousemove", moveDialog);
-
-                let key: string;
-                switch (location.hostname) {
-                    case "boards.4chan.org":
-                        key = "4chan Dialog Coordinates";
-                        break;
-                    case "warosu.org":
-                        key = "Warosu Dialog Coordinates";
-                        break;
-                    default:
-                        return;
-                }
-    
-                GM_setValue(key, [ 
-                    playlist.dialog.node.style.top, playlist.dialog.node.style.right, 
-                    playlist.dialog.node.style.bottom, playlist.dialog.node.style.left 
-                ]);
-    
-                break;
-    
-            case "mousedown":
-                if (e.button !== 0) return;
-    
-                e.preventDefault(); // avoid selection while dragging
-                
-                const rect = playlist.dialog.node.getBoundingClientRect();
-    
-                playlist.dialog.size[0] = rect.width;
-                playlist.dialog.size[1] = rect.height;
-    
-                playlist.dialog.cursorPos[0] = e.x - rect.x;
-                playlist.dialog.cursorPos[1] = e.y - rect.y;
-    
-                if (document.getElementById("header-bar")) {
-                    playlist.dialog.header[0] = document.getElementById("header-bar")?.getBoundingClientRect().y || 0;
-                    playlist.dialog.header[1] = document.getElementById("header-bar")?.offsetHeight || 0;
-                    playlist.dialog.header[2] = document.documentElement.classList.contains("fixed");
-                    playlist.dialog.header[3] = document.documentElement.classList.contains("autohide");
-                }
-    
-                playlist.dialog.node.classList.add("dragging");
-                document.addEventListener("mousemove", moveDialog);
-    
-                break;
-        }
-    
-    }
-    
-    function moveDialog(e: MouseEvent) {
-    
-        if (! playlist.dialog || ! playlist.dialog.node) return;
-    
-        e.preventDefault(); // avoid selection while dragging
-    
-        const sW = document.documentElement.clientWidth,
-            sH = document.documentElement.clientHeight,
-            x = e.x - playlist.dialog.cursorPos[0],
-            y = e.y - playlist.dialog.cursorPos[1],
-            w = playlist.dialog.size[0],
-            h = playlist.dialog.size[1],
-            maxX = sW - w;
-    
-        let minY = 0, maxY = sH - h;
-    
-        if (document.getElementById("header-bar")) {
-            const fixed = playlist.dialog.header[2];
-            const autohide = playlist.dialog.header[3];
-
-            if (fixed && ! autohide) {
-                if (playlist.dialog.header[0] < playlist.dialog.header[1]) {
-                    minY += playlist.dialog.header[1];
-                } else {
-                    maxY -= playlist.dialog.header[1];
-                }
-            }
-        }
-    
-        if (y > maxY) {
-            playlist.dialog.node.style.bottom = 0 + "px";
-            playlist.dialog.node.style.removeProperty("top");
-        } else {
-            playlist.dialog.node.style.top = y > minY ? ((y * 100) / sH) + "%" : minY + "px";
-            playlist.dialog.node.style.removeProperty("bottom");
-        }
-        
-        if (x > maxX) {
-            playlist.dialog.node.style.right = 0 + "px";
-            playlist.dialog.node.style.removeProperty("left");
-        } else {
-            playlist.dialog.node.style.left = x > 0 ? ((x * 100) / sW) + "%" : 0 + "px";
-            playlist.dialog.node.style.removeProperty("right");
-        }
-    
-    }
 });
 
 document.addEventListener("4chanXInitFinished", () => {
+
+    playlist.dialog = new Dialog(playlist);
+
     document.addEventListener("ThreadUpdate", ((e: CustomEvent) => {
         if (e.detail[404]) return;
 
@@ -173,6 +57,7 @@ document.addEventListener("4chanXInitFinished", () => {
             playlist.updatePlayer();
         })
     }) as EventListener);
+    
 });
 
 declare global {
@@ -182,10 +67,6 @@ declare global {
 }
 
 unsafeWindow.onYouTubeIframeAPIReady = () => {
-
-    type TracksHistory = {
-        [key: string]: string
-    }
 
     playlist.player = new YT.Player("playlist", {
         width: '512', height: '288',
@@ -198,10 +79,8 @@ unsafeWindow.onYouTubeIframeAPIReady = () => {
     });
 
     function initPlaylist(e: YT.PlayerEvent) {
-        
-        playlist.track = "";
-        playlist.playing = false;
-        playlist.mutated = false;
+
+        playlist.state = 0;
     
         const pages = playlist.toPages();
         playlist.dialog?.updateTabs(pages.length);
@@ -216,9 +95,8 @@ unsafeWindow.onYouTubeIframeAPIReady = () => {
         } else {
             e.target.cuePlaylist(pages[0])
         }
-    
-        playlist.dialog?.node?.classList.remove("hide");
-        playlist.dialog?.toggle?.classList.remove("disabled");
+
+        playlist.dialog?.toggle();
 
     }
     
